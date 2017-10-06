66cab63d892af02c00fa9d286e96b2aabdce8893
pgreze <pierrick.greze@gmail.com>
2017-10-06 21:20:39 +0900

    Reduce scenario name size

--- gen-scenarios.py
+++ gen-scenarios.py
@@ -17,11 +17,11 @@ write("sample", ''.join("""\
 assemble_sample_%(suffix)s {%(common)s
     gradle-args = ["--parallel", "-Pandroid.gradle_plugin=%(agp)s"]
 }
-incremental_sample_app_%(suffix)s {%(common)s
+inc_sample_app_%(suffix)s {%(common)s
     gradle-args = ["--parallel", "-Pandroid.gradle_plugin=%(agp)s"]
     apply-abi-change-to = "projects/app/src/main/java/lib/app/Cls0.java"
 }
-incremental_sample_lib_%(suffix)s {%(common)s
+inc_sample_lib_%(suffix)s {%(common)s
     gradle-args = ["--parallel", "-Pandroid.gradle_plugin=%(agp)s"]
     apply-abi-change-to = "projects/java/src/main/java/lib/java/Cls0.java"
 }
@@ -34,9 +34,9 @@ incremental_sample_lib_%(suffix)s {%(common)s
 # SINGLE_APP
 
 write("single_app", """\
-assemble_single_app {%(common)s
+build {%(common)s
 }
-incremental_single_app {%(common)s
+inc {%(common)s
     apply-abi-change-to = "projects/app/src/main/java/lib/app/Cls9000.java"
 }
 """ % locals())
@@ -44,12 +44,12 @@ incremental_single_app {%(common)s
 # MULTI_3
 
 write("multi_3", """\
-assemble_app_lib {%(common)s
+build {%(common)s
 }
-incremental_app {%(common)s
+inc_app {%(common)s
     apply-abi-change-to = "projects/app/src/main/java/lib/app/Cls0.java"
 }
-incremental_lib1 {%(common)s
+inc_lib {%(common)s
     apply-abi-change-to = "projects/lib1/src/main/java/lib/lib1/Cls0.java"
 }
 """ % locals())
@@ -57,21 +57,21 @@ incremental_lib1 {%(common)s
 # MULTI_10
 
 write("multi_10", """\
-assemble_app_lib {%(common)s
+build {%(common)s
 }
-incremental_app {%(common)s
+inc_app {%(common)s
     apply-abi-change-to = "projects/app/src/main/java/lib/app/Cls0.java"
 }
-incremental_lib1 {%(common)s
+inc_lib1 {%(common)s
     apply-abi-change-to = "projects/lib1/src/main/java/lib/lib1/Cls0.java"
 }
-incremental_lib3 {%(common)s
+inc_lib3 {%(common)s
     apply-abi-change-to = "projects/lib3/src/main/java/lib/lib3/Cls0.java"
 }
-incremental_lib7 {%(common)s
+inc_lib7 {%(common)s
     apply-abi-change-to = "projects/lib7/src/main/java/lib/lib7/Cls0.java"
 }
-incremental_lib9 {%(common)s
+inc_lib9 {%(common)s
     apply-abi-change-to = "projects/lib9/src/main/java/lib/lib9/Cls0.java"
 }
 """ % locals())
@@ -79,21 +79,21 @@ incremental_lib9 {%(common)s
 # MULTI_100
 
 write("multi_100", """\
-assemble_app_lib {%(common)s
+build {%(common)s
 }
-incremental_app {%(common)s
+inc_app {%(common)s
     apply-abi-change-to = "projects/app/src/main/java/lib/app/Cls0.java"
 }
-incremental_lib19 {%(common)s
+inc_lib19 {%(common)s
     apply-abi-change-to = "projects/lib19/src/main/java/lib/lib19/Cls0.java"
 }
-incremental_lib39 {%(common)s
+inc_lib39 {%(common)s
     apply-abi-change-to = "projects/lib39/src/main/java/lib/lib39/Cls0.java"
 }
-incremental_lib79 {%(common)s
+inc_lib79 {%(common)s
     apply-abi-change-to = "projects/lib79/src/main/java/lib/lib79/Cls0.java"
 }
-incremental_lib99 {%(common)s
+inc_lib99 {%(common)s
     apply-abi-change-to = "projects/lib99/src/main/java/lib/lib99/Cls0.java"
 }
 """ % locals())
